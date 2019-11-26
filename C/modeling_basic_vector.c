07f7c13ea5e5a510cb4d093670c7cf08914ea1cb
fetim <felipetimoteo@id.uff.br>
2019-11-26 00:06:15 -0300

    Created C code

---
+++ C/modeling_basic_vector.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <omp.h>
+
+int main()
+{
+    printf("\nAcoustic wave modelling using the velocities model of stretch 2 - Volta Redonda project\n\n");
+}
